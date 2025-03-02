8cbbcd6d4577a93a654ed6b54c1cc3f7b9f3687a
Andri Gunawan A <andri.gunawan.a87@gmail.com>
2025-03-02 15:09:27 +0700

    End Of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors();
     
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder.AllowAnyOrigin()
+                                .AllowAnyMethod()
+                                .AllowAnyHeader());
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
